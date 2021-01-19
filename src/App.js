76871e401acfadcd2286f7c1066c8fdface86edf
royweb <xytank@gmail.com>
2021-01-19 13:54:46 -0500

    Set up video component and add styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
+
 import './App.css';
+import Video from './components/Video';
 
 function App() {
   return (
     <div className="app">
       <h1>WRI SmVideo</h1>
+
+      <div className="app__videos">
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+      </div>
     </div>
   );
 }
