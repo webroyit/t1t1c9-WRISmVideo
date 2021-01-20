b657d3edbc91483826749ba28158935fd85dd0e0
royweb <xytank@gmail.com>
2021-01-20 14:02:02 -0500

    Use useRef to get a mutable ref object of the element

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import './Video.css';
 
 function Video() {
+    // 'useRef' to access the video
+    const videoRef = useRef(null);
+
+    const onVideoPress = () => {
+        // 'current' to get the curent reference of the video
+        // Play the video
+        videoRef.current.play();
+    }
+
     return (
         <div className="video">
             <iframe
@@ -10,6 +19,8 @@ function Video() {
                 className="video__player"
                 frameborder="0"
                 loop
+                onClick={onVideoPress}
+                ref={videoRef}
                 src="//player.vimeo.com/video/394580451?title=0&amp;portrait=0&amp;byline=0&amp;autoplay=1"
             ></iframe>
         </div>
