dfe099f71c6ea4e99c4d136a88cc54802110f194
royweb <xytank@gmail.com>
2021-01-21 13:59:06 -0500

    Install react-ticker for animation of moving text

--- src/components/VideoFooter.js
+++ src/components/VideoFooter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Ticker from 'react-ticker';
 import MusicNoteIcon from '@material-ui/icons/MusicNote';
 
 import './VideoFooter.css';
@@ -9,7 +10,16 @@ function VideoFooter() {
              <div className="videoFooter__text">
                 <h3>@username1</h3>
                 <p>This is a description</p>
-                <MusicNoteIcon />
+                <div className="videoFooter__ticker">
+                    <MusicNoteIcon className="videoFooter__icon" />
+                    <Ticker mode="smooth">
+                        {({ index }) => (
+                            <>
+                                <p>This is fun, let try it again</p>
+                            </>
+                        )}
+                    </Ticker>
+                </div>
             </div>
             <img
                 className="videoFooter__record"
